5ac5df84400b6e7266a8bdcdee223cc2522a295f
Jason Prickett <prickettjw@gmail.com>
2019-07-23 18:49:40 -0700

    portfolio timeline skeleton

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Typewrite text in header
 const titles = [' Front End Web Developer', 'n Open Source Advocate', ' Back End Web Developer', ' Designer', ' JavaScript Fanboy', ' React Developer', ' Tinkerer', ' Musician'];
 new Typewriter('.header__title', {
     strings: titles,
@@ -5,12 +6,16 @@ new Typewriter('.header__title', {
     loop: true
 });
 
+
+// Create our navigation bar clone to be shown when scrolled past header
 let nav = document.querySelector('nav');
 let navClone = nav.cloneNode(true);
 navClone.classList.add('sticky');
 document.querySelector('body').insertBefore(navClone, nav);
 
-window.onscroll = function(e) {
+
+//Show navigation bar if we're scrolled past it, hide otherwise
+function showNav() {
     page = window.pageYOffset;
     if(page >= window.innerHeight + 50) {
         navClone.classList.add('active');
@@ -21,6 +26,13 @@ window.onscroll = function(e) {
     }
 }
 
+showNav();
+window.onscroll = e => {
+    showNav();
+}
+
+
+// Smooth scroll to anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -32,4 +44,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
+// Timeline navigator
+
+const years = document.querySelectorAll('.portfolio__timeline__marker');
+let activeYear = '2016';
+
+function setActive() {
+    years.forEach(year => {
+        year.classList.remove('marker-active');
+    });
+    Year = this.children[0].dataset.year;
+    this.classList.add('marker-active');
+}
+
+years.forEach(year => {
+    year.addEventListener('click', setActive);
+})
 
